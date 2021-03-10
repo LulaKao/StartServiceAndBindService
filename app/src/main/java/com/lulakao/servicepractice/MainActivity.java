41bd14a854053e47d655715c55c950d246f3b8e7
LulaKao <windandsprings@gmail.com>
2021-03-10 15:07:49 +0800

    Add bindService() method

--- app/src/main/java/com/lulakao/servicepractice/MainActivity.java
+++ app/src/main/java/com/lulakao/servicepractice/MainActivity.java
@@ -1,26 +1,47 @@
 package com.lulakao.servicepractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    private Intent intent_service;
+    // this is for startService() and stopService()
+    private Intent intent_start;
 
+    // this is for bindService() and unbindService()
+    private Intent intent_bind;
+    private MyService.MyBinder myBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            myBinder = (MyService.MyBinder) service;
+            myBinder.startTask();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
+
+    //====== onCreate() START ======//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
+    //====== onCreate() END ======//
 
     //====== onClick : Call to startService() START ======//
     public void startService(View view) {
-        intent_service = new Intent(this, MyService.class);
-        Log.d("MyService","startService --- intent_service = " + intent_service);
-        startService(intent_service);
+        intent_start = new Intent(this, MyService.class);
+        Log.d("MyService","startService --- intent_start = " + intent_start);
+        startService(intent_start);
 
         /** 當 Service 使用 startService() 啟動後，就算 Activity 被關閉，Service 也會持續在 Background 工作。
          *  Service 與 Activity 之間沒有交集，Service 完成任務也不會回傳東西給原來的應用程式。
@@ -32,8 +53,29 @@ public class MainActivity extends AppCompatActivity {
 
     //====== onClick : Call to stopService() START ======//
     public void stopService(View view) {
-        Log.d("MyService","stopService --- intent_service = " + intent_service);
-        stopService(intent_service);
+        Log.d("MyService","stopService --- intent_start = " + intent_start);
+        if(intent_start != null) { // 加上此判斷式，才不會因用戶點了 Start 的按鈕，但 intent_start = null 而 shutdown
+            stopService(intent_start);
+            intent_start = null; // 使用完畢後指定 intent_start = null 以利 GC 回收
+        }
     }
     //====== onClick : Call to stopService() END ======//
+
+    //====== onClick : Call to bindService() START ======//
+    public void bindService(View view) {
+        intent_bind = new Intent(this, MyService.class);
+        Log.d("MyService","bindService --- intent_bind = " + intent_bind);
+        bindService(intent_bind, connection, BIND_AUTO_CREATE);
+    }
+    //====== onClick : Call to bindService() END ======//
+
+    //====== onClick : Call to unbindService() START ======//
+    public void unbindService(View view) {
+        Log.d("MyService","unbindService --- intent_bind = " + intent_bind);
+        if(intent_bind != null) { // 加上此判斷式，才不會因用戶點了 Unbind 的按鈕，但 intent_bind = null 而 shutdown
+            unbindService(connection);
+            intent_bind = null; // 使用完畢後指定 intent_bind = null 以利 GC 回收
+        }
+    }
+    //====== onClick : Call to unbindService() END ======//
 }
--- app/src/main/java/com/lulakao/servicepractice/MyService.java
+++ app/src/main/java/com/lulakao/servicepractice/MyService.java
@@ -2,12 +2,14 @@ package com.lulakao.servicepractice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 
 public class MyService extends Service {
+    private MyBinder myBinder = new MyBinder();
 
     @Override
     public void onCreate() {
@@ -23,25 +25,47 @@ public class MyService extends Service {
         Log.d("MyService", "=== onStartCommand() ===");
         // Do the task below
         // ...
-        Toast.makeText(this, "Start Service...", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "onStartCommand : Start Service", Toast.LENGTH_SHORT).show();
         return super.onStartCommand(intent, flags, startId);
     }
     //====== Call to startService() END ======//
+
+    @Override
+    public void onDestroy() {
+        // The service is no longer used and is being destroyed
+        Log.d("MyService", "=== onDestroy() ===");
+        Toast.makeText(this, "Stop Service", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
 
     //====== Call to bindService() START ======//
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         Log.d("MyService", "=== onBind() ===");
-        return null;
+        return myBinder;
     }
-    //====== Call to bindService() END ======//
 
     @Override
-    public void onDestroy() {
-        // The service is no longer used and is being destroyed
-        super.onDestroy();
-        Log.d("MyService", "=== onDestroy() ===");
-        Toast.makeText(this, "Stop Service", Toast.LENGTH_SHORT).show();
+    public boolean onUnbind(Intent intent) {
+        Log.d("MyService", "=== onUnbind() ===");
+        return super.onUnbind(intent);
     }
+
+    @Override
+    public void onRebind(Intent intent) {
+        Log.d("MyService", "=== onRebind() ===");
+        super.onRebind(intent);
+    }
+
+    class MyBinder extends Binder{
+
+        public void startTask(){
+            Log.d("MyService", "=== MyBinder.startTask() ===");
+            // Do the task below
+            // ...
+            Toast.makeText(MyService.this, "MyBinder.startTask : Start Task", Toast.LENGTH_SHORT).show();
+        }
+    }
+    //====== Call to bindService() END ======//
 }
